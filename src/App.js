c2d088796a10cb502ecf0cc82474d64c7d76fb09
chiru381 <chirukosanam123@gmail.com>
2022-07-01 18:44:49 +0530

    react table

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './App.css'
+/* import { BasicTable } from './components/BasicTable' */
+/* import { RowSelection } from './components/RowSelection'; */
+/* import { PaginationTable } from './components/PaginationTable'; */
+/* import ColoumnOrder from './components/ColumnOrder'; */
+/* import ColumnHiding from './components/ColumnHiding'; */
+/* import StickyTable from './components/StickyTable'; */
+/* import SortingTable from './components/SortingTable'; */
+import FilteringTable from './components/FiltertingTable';
+
+function App() {
+  return (
+    <div className='App'>
+      {/* <BasicTable /> */}
+      {/* <RowSelection /> */}
+      {/* <PaginationTable /> */}
+      {/* <ColoumnOrder /> */}
+      {/* <ColumnHiding /> */}
+      {/* <StickyTable /> */}
+      {/* <SortingTable /> */}
+      <FilteringTable />
+    </div>
+  )
+}
+
+export default App;
